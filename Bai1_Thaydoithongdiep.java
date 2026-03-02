ddbb4226a0e22d9c566b7a238abaaf295ac68965
trangnny66cntt-coder <trang.nny.66cntt@ntu.edu.vn>
2026-03-02 09:17:10 +0700

    Thay đổi nội dung

--- Bai1_Thaydoithongdiep.java
+++ Bai1_Thaydoithongdiep.java
@@ -3,9 +3,9 @@ public class Bai1_Thaydoithongdiep {
 
 	public static void main(String[] args) {
 		System.out.println("Xin chao moi nguoi");
-		System.out.println("Toi ten la Le Minh Trung");
+		System.out.println("Toi ten la Nguyễn Ngọc Yến Trang");
 		System.out.println("Toi hoc mon Lap trinh Java lop 66.CNTT-2");
-		System.out.print("MSSV: 66134203");
+		System.out.print("MSSV: 66134066");
 
 	}
 
