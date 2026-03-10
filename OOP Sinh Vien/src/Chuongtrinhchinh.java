4fd487ec0e307cf2a38da8adbb5f20f7cf06fd4d
trangnny66cntt-coder <trang.nny.66cntt@ntu.edu.vn>
2026-03-10 20:02:00 +0700

    Tạo file chương trình chính

---
+++ OOP Sinh Vien/src/Chuongtrinhchinh.java
@@ -0,0 +1,11 @@
+public class Chuongtrinhchinh {
+    public static void main(String[] args) {
+        // Tạo mới 1 sinh viên
+        Sinhvien sv1 = new Sinhvien();
+        // cấp/gán dữ liệu cụ thể
+        sv1.mssv="66112233";
+        sv1.ten="Nguyễn Lê";
+        sv1.tuoi=18;
+        sv1.hocBai("Lập trình Java");
+    }
+}
