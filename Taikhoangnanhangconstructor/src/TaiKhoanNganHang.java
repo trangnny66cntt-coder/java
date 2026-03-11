716489233ca8342d636c3a583d244c289d875e8b
trangnny66cntt-coder <trang.nny.66cntt@ntu.edu.vn>
2026-03-11 21:43:42 +0700

    Tạo các thuộc tính

--- Taikhoangnanhangconstructor/src/TaiKhoanNganHang.java
+++ Taikhoangnanhangconstructor/src/TaiKhoanNganHang.java
@@ -1,2 +1,56 @@
 public class TaiKhoanNganHang {
+    private String soTaiKhoan;
+    private String tenChuTaiKhoan;
+    private double soDu
+
+    // Constructor 1: Chỉ có số tài khoản
+    public TaiKhoanNganHang(String soTaiKhoan) {
+        this.soTaiKhoan = soTaiKhoan;
+        this.tenChuTaiKhoan = "Chưa xác định";
+        this.soDu = 0.0;
+    }
+
+    // Constructor 2: Có số tài khoản và tên chủ
+    public TaiKhoanNganHang(String soTaiKhoan, String tenChuTaiKhoan) {
+        this.soTaiKhoan = soTaiKhoan;
+        this.tenChuTaiKhoan = tenChuTaiKhoan;
+        this.soDu = 0.0;
+    }
+
+    // Constructor 3: Đầy đủ thông tin
+    public TaiKhoanNganHang(String soTaiKhoan, String tenChuTaiKhoan, double soDu) {
+        this.soTaiKhoan = soTaiKhoan;
+        this.tenChuTaiKhoan = tenChuTaiKhoan;
+        this.soDu = (soDu >= 0) ? soDu : 0.0;
+    }
+    // Getter/Setter
+    public String getSoTaiKhoan() { return soTaiKhoan; }
+    public String getTenChuTaiKhoan() { return tenChuTaiKhoan; }
+    public double getSoDu() { return soDu; }
+
+    public void setTenChuTaiKhoan(String ten) {
+        if (ten != null && ten.length() > 0) {
+            this.tenChuTaiKhoan = ten;
+        }
+    }
+    // Phương thức gửi tiền
+    public boolean guiTien(double soTien) {
+        if (soTien > 0) {
+            this.soDu += soTien;
+            System.out.println("Gửi tiền thành công! Số dư mới: " + this.soDu);
+            return true;
+        }
+        System.out.println("Số tiền gửi phải > 0!");
+        return false;
+    }
+    // Phương thức rút tiền
+    public boolean rutTien(double soTien) {
+        if (soTien > 0 && soTien <= this.soDu) {
+            this.soDu -= soTien;
+            System.out.println("Rút tiền thành công! Số dư mới: " + this.soDu);
+            return true;
+        }
+        System.out.println("Số tiền rút không hợp lệ!");
+        return false;
+    }
 }
