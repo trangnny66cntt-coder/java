e42916946974ecca2c53c65102338f43e79c050d
trangnny66cntt-coder <trang.nny.66cntt@ntu.edu.vn>
2026-03-17 15:43:40 +0700

    in ra man hinh

---
+++ ArraylistOOP/src/runMain.java
@@ -0,0 +1,42 @@
+        import java.util.ArrayList;
+		import java.util.Scanner;
+
+public class runMain {
+
+	public static void main(String[] args) {
+		
+	
+		        ArrayList<Integer> dssonguyen = new ArrayList<Integer>();
+		        Scanner banphim = new Scanner(System.in);
+
+		        for(int i = 0 ; i< 10; i++){
+		            System.out.print("Moi ban nhap phan tu thu " + i + ": ");
+		            Integer tam = banphim.nextInt();
+		            dssonguyen.add(tam);
+
+		        }
+		        // b. in ra màn hình
+		        System.out.print("Danh sach so nguyen la: ");
+		        for ( Integer x : dssonguyen){
+		            System.out.print(x + " ");
+		        }
+		        // c. kiêểm tra bao số chẵn
+		        int tam = 0;
+		        for ( Integer x : dssonguyen){
+		            if( x % 2 == 0 ) {
+		                tam++;
+		            }
+		        }
+		        System.out.println("\n");
+		        System.out.println("Co " + tam + " so chan trong mang ");
+		        // d. tppmgr cac phan tu
+		        int temp = 0;
+		        for ( Integer x : dssonguyen){
+		            temp += x;
+		        }
+		        System.out.println("Tong cac phan tu trong mang la: " + temp + " ");
+		    }
+
+	}
+
+
