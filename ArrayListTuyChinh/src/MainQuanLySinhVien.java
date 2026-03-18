ff6a72acc4a2f67ab64455289602e1d625111ea6
trangnny66cntt-coder <trang.nny.66cntt@ntu.edu.vn>
2026-03-18 23:32:33 +0700

    sinh viên

---
+++ ArrayListTuyChinh/src/MainQuanLySinhVien.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Lớp SinhVien
+class SinhVien {
+    private String maSV;
+    private String hoTen;
+    private double diemTB;
+    
+    public SinhVien(String maSV, String hoTen, double diemTB) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.diemTB = diemTB;
+    }
+    
+    // Getter
+    public String getMaSV() { return maSV; }
+    public String getHoTen() { return hoTen; }
+    public double getDiemTB() { return diemTB; }
+    
+    @Override
+    public String toString() {
+        return String.format("Mã: 
+    }
+}
+public class MainQuanLySinhVien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
