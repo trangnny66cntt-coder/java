52a875621ce82ed830b4e09100e096b4bd85062b
trangnny66cntt-coder <trang.nny.66cntt@ntu.edu.vn>
2026-03-19 10:32:08 +0700

    xong nhân viên văn phòng + test

---
+++ ViduOOP_Kethua_QLNV/src/nnyt/quanlynhanvien/NhanVienVanPhong.java
@@ -0,0 +1,29 @@
+package nnyt.quanlynhanvien;
+
+public class NhanVienVanPhong extends NhanVien{
+private double soGioLamThem;
+private double luongThemMoiGio;
+// Hàm tạo
+public NhanVienVanPhong(String maNV, String hoTen, double LuongCoBan, double soGioLamThem, double LuongThemMoiGio) {
+super(maNV, hoTen, luongCoBan);
+this.soGioLamThem = soGioLamThem;
+this.luongThemMoiGio = luongThemMoiGio;
+ }
+@Override
+public double tinhLuong() {
+	return super.  getluongCoBan() + soGioLamThem*luongThemMoiGio;
+}
+public void hienThiThongTin() {
+	super.hienThiThongTin();
+	System.out.println("Số giờ làm thêm: " + soGioLamThem);
+	System.out.println("Lương cho mỗi giờ làm thêm: " + luongThemMoiGio);
+	System.out.println("Lương được nhận: " + tinhLuong());
+}
+public String toString() {
+	String chuoiXuat= super.toString();
+	chuoiXuat = chuoiXuat + "\nSố giờ làm thêm: " + soGioLamThem;
+	chuoiXuat = chuoiXuat + "\nLương cho mỗi giờ làm thêm: " + luongThemMoiGio;
+	chuoiXuat = chuoiXuat + "\nLương được nhận: " + tinhLuong();
+	return chuoiXuat;
+
+}
