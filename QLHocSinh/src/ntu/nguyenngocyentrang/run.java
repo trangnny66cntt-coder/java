38dc9b651dadf6ab7f76f9310058590d26e1e8db
trangnny66cntt-coder <trang.nny.66cntt@ntu.edu.vn>
2026-04-02 00:54:48 +0700

    QLHocSinh

--- QLHocSinh/src/ntu/nguyenngocyentrang/run.java
+++ QLHocSinh/src/ntu/nguyenngocyentrang/run.java
@@ -28,13 +28,76 @@ public class run {
 		
 		HocSinh hs3 = new HocSinh();
 		
+		System.out.print("Mời nhập tên học sinh thứ 3: ");
+		String tenHS3 = banPhim.nextLine();
+		hs3.setTenHS(tenHS3);
+		
 		System.out.print("Mời nhập tuổi học sinh thứ 3: ");
-		String tuoiHS3 = banPhim.nextLine();
-		hs3.setTenHS(tuoiHS3);
+		Short tuoiHS3 = banPhim.nextShort();
+		hs3.setTuoiHS(tuoiHS3);
 		
 		System.out.print("Mời nhập lớp học sinh thứ 3: ");
 		String lopHS3 = banPhim.nextLine();
-		hs3.setTenHS(lopHS3);
+		hs3.setLopHS(lopHS3);
+		
+		System.out.print(hs3.toString());
+		
+		ArrayList<HocSinh> dsHocSinh = new ArrayList<HocSinh>();
+		
+		int N = 3;
+		for (int i = 0; i<N;i++);{
+        HocSinh hsi = new HocSinh();
+        
+        System.out.print("Mời nhập tên học sinh thứ: " +i);
+    	String tenHSi = banPhim.nextLine();
+		hsi.setTenHS(tenHSi);
+		
+		System.out.print("Mời nhập tuổi học sinh thứ: " + i );
+		Short tuoiHSi = banPhim.nextShort();
+		hsi.setTuoiHS(tuoiHSi);
+		
+		System.out.print("Mời nhập lớp học sinh thứ : " + i);
+		String lopHSi = banPhim.nextLine();
+		hsi.setLopHS(lopHSi);
+		
+		// dua vao danh sach
+		dsHocSinh.add(hsi);
+		
 	}
+	//in ra
+	for (int i = 0; i<N;i++) {
+		System.out.println(dsHocSinh.get(i).toString());
+	}
+	HocSinh hsThem = new HocSinh();
+	System.out.print("Mời nhập tên học sinh thêm: ");
+	String tenHSThem = banPhim.nextLine();
+	hsThem.setTenHS(tenHSThem);
+	
+	System.out.print("Mời nhập tuổi học sinh thêm: ");
+	Short tuoiHSThem = banPhim.nextShort();
+	hsThem.setTuoiHS(tuoiHSThem);
+	
+	System.out.print("Mời nhập lớp học sinh thêm: ");
+	String lopHSThem = banPhim.nextLine();
+	hs3.setLopHS(lopHSThem);
 
+	dsHocSinh.add(hsThem);
+	
+	for(int i=0; i<N; i++) {
+		System.out.println(dsHocSinh.get(i).toString());
+	}
+	
+	// 5a
+	for(int i=0; i<N; i++) {
+		HocSinh hsTim = dsHocSinh.get(i);
+		if(hsTim.getTenHS()=="Hoa") {
+			dsHocSinh.remove(hsTim);
+			break;
+		}
+	}
+	//in ra
+	for(int i=0; i<N; i++) {
+		System.out.println(dsHocSinh.get(i).toString());
+	}
+ }
 }
