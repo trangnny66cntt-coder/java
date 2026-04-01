fa304461371a687a3775add21133a5a94e312352
trangnny66cntt-coder <trang.nny.66cntt@ntu.edu.vn>
2026-04-01 19:02:43 +0700

    ham tao

---
+++ QLHocSinh/src/ntunguyenngocyentrang/HocSinh.java
@@ -0,0 +1,38 @@
+package ntunguyenngocyentrang;
+
+public class HocSinh {
+	private String tenHS;
+	private short tuoiHS;
+	private String lopHS;
+	public HocSinh() {
+		
+	}
+	
+	public HocSinh(String tenHS, short tuoiHS, String lopHS) {
+		super();
+		this.tenHS = tenHS;
+		this.tuoiHS = tuoiHS;
+		this.lopHS = lopHS;
+	}
+
+	public String getTenHS() {
+		return tenHS;
+	}
+	public void setTenHS(String tenHS) {
+		this.tenHS = tenHS;
+	}
+	public short getTuoiHS() {
+		return tuoiHS;
+	}
+	public void setTuoiHS(short tuoiHS) {
+		this.tuoiHS = tuoiHS;
+	}
+	public String getLopHS() {
+		return lopHS;
+	}
+	public void setLopHS(String lopHS) {
+		this.lopHS = lopHS;
+	}
+	
+
+}
